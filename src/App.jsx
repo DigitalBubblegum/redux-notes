2d83a5c02d46a43b673fc10a9b824f99d2042e9f
DigitalBubblegum <ghoshaldiwakar@gmail.com>
2023-10-14 15:09:51 +0530

    -pure functions, testing and deepFreeze

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import { createStore } from 'redux'
-const noteReducer = (state = [], action) => {
-  if (action.type === 'NEW_NOTE') {
-    state.concat(action.payload)
-    return state
-  }
-
-  return state
-}
+import noteReducer from "./reducers/noteReducer"
 
 const store = createStore(noteReducer)
 
