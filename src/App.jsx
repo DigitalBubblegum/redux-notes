7265d1830d959ca0d5a9810cafb1c7195d3e1b5c
DigitalBubblegum <ghoshaldiwakar@gmail.com>
2023-10-18 19:30:49 +0530

    -small fixes

--- src/App.jsx
+++ src/App.jsx
@@ -2,37 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createNote, toggleImportanceOf } from './reducers/noteReducer'
 import { useSelector, useDispatch } from 'react-redux'
-import { createStore } from 'redux'
-import noteReducer from "./reducers/noteReducer"
 
-const store = createStore(noteReducer)
-
-store.dispatch({
-  type: 'NEW_NOTE',
-  payload: {
-    content: 'the app state is in redux store',
-    important: true,
-    id: 1
-  }
-})
-
-store.dispatch({
-  type: 'NEW_NOTE',
-  payload: {
-    content: 'state changes are made with actions',
-    important: false,
-    id: 2
-  }
-})
-
-store.dispatch({
-  type: 'NEW_NOTE',
-  payload: {
-    content: 'state changes are made',
-    important: false,
-    id: 3
-  }
-})
 const App = () => {
   const dispatch = useDispatch()
   const notes = useSelector(state => state)
