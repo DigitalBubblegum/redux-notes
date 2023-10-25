0346806094b2cbc66ee5842dd53cd581571cba77
DigitalBubblegum <ghoshaldiwakar@gmail.com>
2023-10-25 19:21:40 +0530

    combined reducers and filters

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
 import NewNote from './components/NewNote'
 import Notes from './components/Notes'
-
+import VisibilityFilter from './components/VisibilityFilter'
 const App = () => {
+  const filterSelected = (value) => {
+    console.log(value)
+  }
   return (
     <div>
       <NewNote />
+      <VisibilityFilter />
       <Notes />
     </div>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { createStore } from 'redux'
+import { createStore,combineReducers } from 'redux'
 import { Provider } from 'react-redux'
-import App from './App.jsx'
+import App from './App'
+
 import noteReducer from './reducers/noteReducer'
-const store = createStore(noteReducer)
+import filterReducer from './reducers/filterReducer.js'
+
+import { createNote } from './reducers/noteReducer'
+import { filterChange } from './reducers/filterReducer'
+
+const reducer = combineReducers({
+  notes: noteReducer,
+  filter: filterReducer
+})
+
+const store = createStore(reducer)
+
+console.log(store.getState())
+store.subscribe(() => console.log(store.getState()))
+store.dispatch(filterChange('IMPORTANT'))
+store.dispatch(createNote('combineReducers forms one reducer from many simple reducers'))
 const root = ReactDOM.createRoot(document.getElementById('root'))
 const renderApp = () => root.render(
   <Provider store={store}>
