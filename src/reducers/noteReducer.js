7e8a862b93307fd8c8a02f7745ace1d76ab175d4
DigitalBubblegum <ghoshaldiwakar@gmail.com>
2023-11-02 14:58:15 +0530

    -redux toolkit

--- src/reducers/noteReducer.js
+++ src/reducers/noteReducer.js
@@ -1,3 +1,4 @@
+import { createSlice } from "@reduxjs/toolkit"
 const initialState = [
   {
     content: 'reducer defines how redux store works',
@@ -10,50 +11,33 @@ const initialState = [
     id: 2,
   },
 ]
-console.log('test',initialState)
-const noteReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case "NEW_NOTE":
-      console.log(state)
-      return [...state, action.payload]
-    case "TOGGLE_IMPORTANCE": {
-      console.log('toggling importance',state)
-      const id = action.payload.id;
-      console.log('id is',id)
-      console.log('notes in state',state)
-      const noteToChange = state.find(n=>n.id === id)
-      console.log('note being changed',noteToChange)
-      const changedNote = {
-        ...noteToChange,
-        important: !noteToChange.important,
-      }
-      console.log('new  note is',changedNote)
-      var notesList = state.map((note) =>
-        note.id !== id ? note : changedNote
-      )
-      console.log('modded note list',notesList)
-      return notesList
-    }
-    default:
-      return state;
-  }
-}
+
 const generateId = () => Number((Math.random() * 1000000).toFixed(0));
 
-export const createNote = (content) => {
-  return {
-    type: "NEW_NOTE",
-    payload: {
-      content,
-      important: false,
-      id: generateId(),
+const noteSlice = createSlice({
+  name: 'notes',
+  initialState,
+  reducers: {
+    createNote(state, action) {
+      const content = action.payload
+      state.push({
+        content,
+        important: false,
+        id: generateId(),
+      })
     },
-  };
-};
-export const toggleImportanceOf = (id) => {
-  return {
-    type: "TOGGLE_IMPORTANCE",
-    payload: { id },
-  };
-};
-export default noteReducer;
+    toggleImportanceOf(state, action) {
+      const id = action.payload
+      const noteToChange = state.find(n => n.id === id)
+      const changedNote = { 
+        ...noteToChange, 
+        important: !noteToChange.important 
+      }
+      return state.map(note =>
+        note.id !== id ? note : changedNote 
+      )     
+    }
+  },
+})
+export const { createNote, toggleImportanceOf } = noteSlice.actions
+export default noteSlice.reducer
